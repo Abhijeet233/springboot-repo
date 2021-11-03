743ab9287dca497fa9daa05d97728e1f70c9b5f6
abhijeet233 <abhijeetdhariwal199@gmail.com>
2021-11-03 15:48:58 +0530

    adding user serive

---
+++ user_service/user_service/src/main/java/com/user/user_service/entity/User.java
@@ -0,0 +1,32 @@
+package com.user.user_service.entity;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class User  {
+    private Long userId;
+    private String name;
+    private String phone;
+
+    List<Contact> contacts = new ArrayList<>();
+
+
+    public User(Long userId, String name, String phone) {
+        this.userId = userId;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public User() {
+    }
+
+    public User(Long userId, String name, String phone, List<Contact> contacts) {
+        this.userId = userId;
+        this.name = name;
+        this.phone = phone;
+        this.contacts = contacts;
+    }
+}
